9427343ffd004fda7b3594aac833cba925393fea
DiegoGp227 <DiegoGp227@gmail.com>
2025-05-14 01:50:49 -0500

    feat: add initial database structure

---
+++ backend/db/db.sql
@@ -0,0 +1,34 @@
+-- Crear la base de datos
+CREATE DATABASE taskly;
+
+-- Seleccionar la base de datos para usarla
+USE taskly;
+
+-- Crear la tabla de usuarios
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Crear la tabla de proyectos (temas)
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Crear la tabla de tareas
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    project_id INT,
+    title VARCHAR(255) NOT NULL,
+    priority ENUM('low', 'medium', 'high') NOT NULL,
+    status ENUM('todo', 'in_progress', 'done') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+);
