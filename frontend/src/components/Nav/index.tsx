b72fb2bb951720e92bb2a4643c496798edf7ef38
kaleth.dev <dev.kaleth.mendoza@gmail.com>
2025-05-16 20:50:32 -0500

    [ marge master ]

--- frontend/src/components/Nav/index.tsx
+++ frontend/src/components/Nav/index.tsx
@@ -11,7 +11,7 @@ const Nav = () => {
 
                     </li>
                     <li>
-                        <p>create tag</p>
+                        <p>create ta</p>
                     </li>
                     <li className={Style.li_user}>
                         <svg width="38" height="38" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
