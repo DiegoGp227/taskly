be11385ae8961fb55796504379a4b748067edd45
kaleth.dev <dev.kaleth.mendoza@gmail.com>
2025-05-16 20:22:36 -0500

    [ home ] - v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Routes , Route } from 'react-router-dom'
 import Login from './components/Login/index'
+import Home from './components/Home/index'
 function App() {
   return (
     <>
       <Routes>
         <Route path='/login' Component={Login}/>
+        <Route path='/home' Component={Home} />
       </Routes> 
     </>
   )
