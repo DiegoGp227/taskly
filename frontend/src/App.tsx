7a3e7efdd52d71d0f3f0d176ed482a403ccd55e0
DiegoGp227 <DiegoGp227@gmail.com>
2025-06-03 00:57:00 -0500

    [ feat ] Page not found added and integrated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
-import './App.css'
-import { Routes , Route } from 'react-router-dom'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login/LoginPage.tsx";
-import HomePage from './pages/Home/HomePage.tsx';
-import TaskPage from './pages/Tasks/TasksPage.tsx'
-import './style/style.css'
+import HomePage from "./pages/Home/HomePage.tsx";
+import TaskPage from "./pages/Tasks/TasksPage.tsx";
+import NotFound from "./pages/NotFound/NotFound.tsx";
+import "./style/style.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/login' Component={LoginPage}/>
-        <Route path='/home' Component={HomePage}/>
-        <Route path='/tasks' Component={TaskPage} />
-      </Routes> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/tasks" element={<TaskPage />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
