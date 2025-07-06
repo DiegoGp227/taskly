2887e40e6d04d4af36464c6bde45c71c9e21df84
DiegoGp227 <diego.gp227@gmail.com>
2025-07-06 01:10:39 -0500

    [ style ] form of the new topic stylized

--- frontend/src/components/Home/NewTopicDiv/index.tsx
+++ frontend/src/components/Home/NewTopicDiv/index.tsx
@@ -3,16 +3,11 @@ import style from "./style.module.css";
 function NewTopicsDiv() {
   return (
     <div className={style.generalDiv}>
-        <h1>
-            Add new topic
-        </h1>
-      <form action="">
-        <label htmlFor="">temas</label>
-        <input type="text" />
-        <label htmlFor="">temas</label>
-        <input type="text" />
-        <label htmlFor="">temas</label>
-        <input type="text" />
+      <h1 className={style.title}>Add new topic</h1>
+      <form action="" className={style.form}>
+        <input type="text" className={style.input} placeholder="Title"/>
+        <textarea name="" id=""placeholder="Description" className={style.textArea}></textarea>
+        <button type="submit" className={style.button}>Send</button>
       </form>
     </div>
   );
